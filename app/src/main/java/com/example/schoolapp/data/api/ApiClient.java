c750f30cc6413d168cb0adef84d44525cae242ce
JOEL <marrinezjoel1@gmail.com>
2025-05-15 21:04:00 -0500

    Cambio de IP en ApiClient.java para conexión con servidor Laravel

--- app/src/main/java/com/example/schoolapp/data/api/ApiClient.java
+++ app/src/main/java/com/example/schoolapp/data/api/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.0.9:8000/api/";
+    private static final String BASE_URL = "http://192.168.0.80:8000/api/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getCliente() {
